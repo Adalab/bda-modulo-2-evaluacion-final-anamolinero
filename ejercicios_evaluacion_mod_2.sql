1ec974be5a74119fe84164dc56a3835fcc0bfbed
Ana Molinero <ana.moli@hotmail.com>
2025-03-12 09:03:03 +0100

    push de prueba

---
+++ ejercicios_evaluacion_mod_2.sql
@@ -0,0 +1,8 @@
+/* EJERCICIOS EVALUACIÓN MÓDULO 2: SQL 
+	CON BBDD SAKILA */
+
+-- 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
+-- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
+-- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
+-- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
+-- 5. Recupera los nombres de todos los actores.
